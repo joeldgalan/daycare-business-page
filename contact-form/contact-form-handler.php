6e24e3303428ca26973c2e96cef20addbe8c0dec
Joel Galan <joeldgalan@gmail.com>
2023-04-12 16:56:20 -0400

    Added index.html, stylesheet, and script.js. Added folders for privacy page, blog posts, contact form, and photos.

---
+++ contact-form/contact-form-handler.php
@@ -0,0 +1,45 @@
+<?php
+    $p1_name = $_POST['p-1-name'];
+    $p1_number = $_POST['p-1-phone-number'];
+    $p1_email = $_POST['p-1-email'];
+    $p2_name = $_POST['p-2-name'];
+    $p2_number = $_POST['p-2-phone-number'];
+    $p2_email = $_POST['p-2-email'];
+    $street_address = $_POST['street-address'];
+    $apt_num = $_POST['apt-num'];
+    $city = $_POST['city'];
+    $state = $_POST['state'];
+    $zip_code = $_POST['zip'];
+    $num_of_children = $_POST['num-of-children'];
+    $names_ages = $_POST['name-and-age'];
+    $start_care = $_POST['start-care'];
+    $allergies = $_POST['allergies'];
+    $subsidy = $_POST['subsidy'];
+    $discover_method = $_POST['discover-method'];
+    $comments = $_POST['comments'];
+
+    $email_from = "yourchildcare_alert@ycc.com";
+    $email_subject = "Alert! New Form Submission";
+    $email_body = "Parent 1 Name: $p1_name\n".
+                    "Parent 1 Phone Number: $p1_number\n".
+                    "Parent 1 Email: $p1_email\n".
+                    "Parent 2 Name: $p2_name\n".
+                    "Parent 2 Phone Number: $p2_number\n".
+                    "Parent 2 Email: $p2_email\n".
+                    "Home Address: $street_address $apt_num $city, $state $zip_code\n".
+                    "Number of Children, Names, and Ages: $num_of_children\n$names_ages\n".
+                    "Desired Start Date: $start_care\n".
+                    "Allergies/Dietary Restrictions: $allergies\n".
+                    "Child Care Scholarship: $subsidy\n".
+                    "Method of Discovery: $discover_method\n".
+                    "Additional Comments: $comments\n";
+
+    $to = "yourchildcare@ycc.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply To: $p1_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: contact-form.html");
+?>
---
+++ script.js
@@ -0,0 +1,115 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.getElementById("nav-links");
+
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("prev-arrow");
+const nextButton = document.getElementById("next-arrow");
+
+const blogContainer = document.getElementById("posts-container");
+const seeMoreBtn = document.getElementById("expand-close-btn");
+
+const navigationBtns = document.querySelectorAll(".manual-btn");
+const radioBtns = document.querySelectorAll(".gal-radio");
+
+// adds .active class to animate hamburger and show nav links. //
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+})
+
+// when a nav link is clicked in hamburger mode, this will close the nav menu //
+document.querySelectorAll(".link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navLinks.classList.remove("active");
+}))
+
+let isHidden = true;
+function showMoreLess() {
+  console.log(blogContainer.style.height);
+  if (isHidden === true) {
+    blogContainer.style.height = "100%";
+    seeMoreBtn.textContent = "See Less"
+    isHidden = false;
+  } else if (isHidden === false) {
+    blogContainer.style.height = "26rem";
+    seeMoreBtn.textContent = "See More"
+    isHidden = true;
+  }
+}
+
+function nextSlide() {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+}
+
+function prevSlide() {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+}
+
+let navClicked = false;
+function navClick() {
+  navClicked = true;
+}
+
+function navClickReset() {
+  navClicked = false;
+} 
+
+let counter = 1;
+setInterval(function(){
+  document.getElementById('gal-radio' + counter).checked = true
+  counter++;
+  if (counter > 4) {
+    counter = 1;
+  }
+  var evt = new CustomEvent('input');
+  radioBtns[0].dispatchEvent(evt);
+  radioBtns[1].dispatchEvent(evt);
+  radioBtns[2].dispatchEvent(evt);
+  radioBtns[3].dispatchEvent(evt);
+}, 5000);
+
+radioBtns.forEach(btn => {
+  btn.addEventListener("input", () => {
+    if (btn.checked === true) {
+      if (btn.id === "gal-radio1") {
+        navigationBtns[0].style.background = "var(--main-blue)";
+        navigationBtns[1].style.background = "none";
+        navigationBtns[2].style.background = "none";
+        navigationBtns[3].style.background = "none";
+      } else if (btn.id === "gal-radio2") {
+        navigationBtns[0].style.background = "none";
+        navigationBtns[1].style.background = "var(--main-blue)";
+        navigationBtns[2].style.background = "none";
+        navigationBtns[3].style.background = "none";
+      } else if (btn.id === "gal-radio3") {
+        navigationBtns[0].style.background = "none";
+        navigationBtns[1].style.background = "none";
+        navigationBtns[2].style.background = "var(--main-blue)";
+        navigationBtns[3].style.background = "none";
+      } else if (btn.id === "gal-radio4") {
+        navigationBtns[0].style.background = "none";
+        navigationBtns[1].style.background = "none";
+        navigationBtns[2].style.background = "none";
+        navigationBtns[3].style.background = "var(--main-blue)";
+      }
+    }
+    console.log(btn.checked);
+  });
+});
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (hamburger.classList.contains("active") === false) {
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
+      navClicked = false;
+    } else if (prevScrollpos < currentScrollPos && navClicked === false) {
+      document.getElementById("navbar").style.top = "-60px";
+    }
+  }
+  prevScrollpos = currentScrollPos;
+}
